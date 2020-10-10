22f4a61322d840acae50fd3ac3176ecce15af2ac
OliverTuesta <oliver.jtuesta@gmail.com>
2020-10-10 17:53:43 -0500

    nuevo layout implementado

--- app/src/main/java/com/example/tresenraya/MainActivity.kt
+++ app/src/main/java/com/example/tresenraya/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.tresenraya
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnRun: Button = findViewById(R.id.btnRun)
+        btnRun.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
